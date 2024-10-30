be8fff4dfa79c95ba583ced4b560f44675fd2ec6
Your Name <you@example.com>
2024-10-30 18:38:06 +0000

    renew241030

--- ohmyzsh/.oh-my-zsh/custom/aliases.zsh
+++ ohmyzsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,14 +1,3 @@
-# Add your own custom alias in the custom/aliases directory. Aliases placed
-# here will override ones with the same name in the main alias directory.
-
-alias dud='du -d 1 -h'                      # Short and human-readable directory listing
-alias duf='du -sh *'                        # Short and human-readable file listing
-
-alias far='far2l --wayland --ee --tty'
-alias fara='far2l --wayland --ee --tty -an'
-alias wfar='far2l --wayland --ee'
-alias wfara='far2l --wayland --ee -an'
-
 alias sudo="sudo -E " # carry over environment to sudo commands
 alias ls='ls --color=auto'
 alias ll='ls -Alh'
